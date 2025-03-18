95bb783c5e873ccb58426a50148e2342fa52cb86
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-18 15:17:22 -0600

    Creation of project with all things.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'basic',
+        title: 'Pipes básicos',
+        loadComponent: () => import('./pages/basic-page/basic-page.component')
+    },
+    {
+        path: 'numbers',
+        title: 'Numbers Pipes',
+        loadComponent: () => import('./pages/numbers-page/numbers-page.component')
+    },
+    {
+        path: 'uncommon',
+        title: 'Pipes no tan comunes',
+        loadComponent: () => import('./pages/uncommon-page/uncommon-page.component')
+    },
+    {
+        path: 'custom',
+        title: 'Pipes personalizados',
+        loadComponent: () => import('./pages/custom-page/custom-page.component')
+    },
+];
