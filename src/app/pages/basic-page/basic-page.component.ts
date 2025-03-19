396fc76fb2043b9bac85652cd6dd455102446235
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-19 12:04:25 -0600

    DatePipe.

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -11,4 +11,17 @@ export default class BasicPageComponent {
   nameLower = signal('jonathan');
   nameUpper = signal('JONATHAN');
   fullName = signal('JoNaThan JuAreZ');
+
+  customDate = signal(new Date());
+
+  //Make an effect using Interval where it gets a new value each 1000ms in cleanup that interval is deleted
+  tickingDateEffect = effect((onCleanup)=> {
+    const interval = setInterval(()=>{
+      this.customDate.set(new Date())
+    }, 1000)
+
+    onCleanup(()=> {
+      clearInterval(interval);
+    })
+  })
 }
