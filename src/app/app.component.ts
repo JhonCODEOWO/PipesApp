b5a4453e5466276511528553480ec8145b04d533
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-19 11:26:56 -0600

    LowerCase and UpperCase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
