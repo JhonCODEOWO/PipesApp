6603af1c70746e6e2dfb4a99db04219b3e6d71d0
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-06-09 11:42:51 -0600

    I18nSelectPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,42 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Alejandra',
+  gender: 'female',
+  age: 39,
+  address: 'Ottawa, Canada'
+}
+
+const client2 = {
+  name: 'Jonathan',
+  gender: 'male',
+  age: 22,
+  address: 'Zacatlán, Puebla'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(){
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+
+    this.client.set(client1);
+  }
+}
